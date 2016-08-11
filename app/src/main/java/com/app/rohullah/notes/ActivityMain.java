bbff05aced084c4e4c93d32f3a7237c2916d97c9
rohullah <rohullahalavi@gmail.com>
2016-08-11 18:59:17 +0200

    ListView with custom adapter

--- app/src/main/java/com/app/rohullah/notes/ActivityMain.java
+++ app/src/main/java/com/app/rohullah/notes/ActivityMain.java
@@ -7,7 +7,7 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
